b5ebbfc49849f654895ccfc346468a9bfccfa876
AndersonN031 <devandersonnunes078@gmail.com>
2026-05-19 08:41:38 -0300

    feat: adding controller

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Put } from '@nestjs/common';
+import { GetMeUseCase } from '../usecases/get-me.usecase';
+import { CurrentUser } from '@shared/decorators/current-user.decorator';
+import type { CurrentUserPayload } from '@shared/decorators/current-user.decorator';
+import { UpdateMeDto } from '../dto/update-me.dto';
+import { UpdateMeUseCase } from '../usecases/update-me.usecase';
 
 @Controller('users')
-export class UsersController {}
+class UsersController {
+  constructor(
+    private readonly getMeUseCase: GetMeUseCase,
+    private readonly updateMeUseCase: UpdateMeUseCase,
+  ) {}
+
+  @Get('me')
+  async getMe(@CurrentUser() user: CurrentUserPayload) {
+    return this.getMeUseCase.execute(user.sub);
+  }
+
+  @Put('me')
+  async updateMe(
+    @CurrentUser() user: CurrentUserPayload,
+    @Body() dto: UpdateMeDto,
+  ) {
+    return this.updateMeUseCase.execute(user.sub, dto);
+  }
+}
+
+export { UsersController };
