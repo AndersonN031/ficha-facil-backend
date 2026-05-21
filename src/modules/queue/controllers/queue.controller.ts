09bce763aa9243b8461ac02b69d68b5fdb7851de
AndersonN031 <devandersonnunes078@gmail.com>
2026-05-21 08:48:17 -0300

    feat: create endpoints of queue.controller

--- src/modules/queue/controllers/queue.controller.ts
+++ src/modules/queue/controllers/queue.controller.ts
@@ -1,6 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { EnterQueueUseCase } from '../usecases/enter-queue.usecase';
+import { LeaveQueueUseCase } from '../usecases/leave-queue.usecase';
+import { CurrentUser } from '@shared/decorators/current-user.decorator';
+import type { CurrentUserPayload } from '@shared/decorators/current-user.decorator';
 
 @Controller('queue')
-class QueueController {}
+class QueueController {
+  constructor(
+    private readonly enterQueueUseCase: EnterQueueUseCase,
+    private readonly leaveQueueUseCase: LeaveQueueUseCase,
+  ) {}
+
+  @Post(':unitId/enter')
+  async enter(
+    @Param('unitId') unitId: string,
+    @CurrentUser() user: CurrentUserPayload,
+  ) {
+    return this.enterQueueUseCase.execute(unitId, user.sub);
+  }
+
+  @Delete(':entryId')
+  @HttpCode(HttpStatus.OK)
+  async leave(
+    @Param('entryId') entryId: string,
+    @CurrentUser() user: CurrentUserPayload,
+  ) {
+    return this.leaveQueueUseCase.execute(entryId, user.sub);
+  }
+}
 
 export { QueueController };
