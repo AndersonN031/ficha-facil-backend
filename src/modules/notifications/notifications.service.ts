456bb1fa775cccc3fd6971db7328d1bd1d6155f5
AndersonN031 <devandersonnunes078@gmail.com>
2026-06-15 08:12:01 -0300

    feat: creating NotificationsService

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -1,6 +1,32 @@
+import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bullmq';
+
+export interface NotifyPatientJob {
+  healthUnitId: string;
+  userId: string;
+  tickerNumber: number;
+  position: number;
+  message: string;
+}
 
 @Injectable()
-class NotificationsService {}
+class NotificationsService {
+  constructor(
+    @InjectQueue('notifications') private readonly notificationsQueue: Queue,
+  ) {}
+
+  async notifyPatientCalled(data: NotifyPatientJob): Promise<void> {
+    await this.notificationsQueue.add('ticket:called', data, {
+      attempts: 3,
+      backoff: {
+        type: 'exponential',
+        delay: 2000,
+      },
+      removeOnComplete: true,
+      removeOnFail: true,
+    });
+  }
+}
 
 export { NotificationsService };
