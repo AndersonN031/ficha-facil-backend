22ed59d33f4098d939f1d2d9cd284d7b85c3f8a4
AndersonN031 <devandersonnunes078@gmail.com>
2026-06-27 08:08:29 -0300

    fix: adding validation healthUnit not found

--- src/modules/users/usecases/find-all-users.usecase.ts
+++ src/modules/users/usecases/find-all-users.usecase.ts
@@ -1,15 +1,21 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UsersRepository } from '../repositories/users.repository';
+import { HealthUnitsRepository } from '@modules/health-units/repositories/health-units.repository';
 
 @Injectable()
 class FindAllUsersUseCase {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly healthUnitsRepository: HealthUnitsRepository,
+  ) {}
 
   async execute(healthUnitId: string) {
-    const user = await this.usersRepository.findAllUsers(healthUnitId);
-    if (!user) {
+    const unit = await this.healthUnitsRepository.findById(healthUnitId);
+    if (!unit) {
       throw new NotFoundException('Posto não encontrado');
     }
+
+    const user = await this.usersRepository.findAllUsers(healthUnitId);
     return user;
   }
 }
