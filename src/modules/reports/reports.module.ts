1ec590a8eec49bb19d87fb90f7cc209df9c29265
AndersonN031 <devandersonnunes078@gmail.com>
2026-07-07 07:56:59 -0300

    feat(module): creating reports module

---
+++ src/modules/reports/reports.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ReportsController } from './controllers/reports.controller';
+import { ReportsRepository } from './repositories/reports.repository';
+import { GetDailyReportUseCase } from './usecases/get-daily-report.usecase';
+import { TicketsModule } from '@modules/tickets/tickets.module';
+
+@Module({
+  imports: [TicketsModule],
+  controllers: [ReportsController],
+  providers: [ReportsRepository, GetDailyReportUseCase],
+})
+export class ReportsModule {}
