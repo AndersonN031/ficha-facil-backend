5ab9fe87010d2d81cae404df9112a8a694b01d1f
AndersonN031 <devandersonnunes078@gmail.com>
2026-09-12 08:44:28 -0300

    feat: integrate sentry for error monitoring with global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import './instrument';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SanitizePipe } from '@shared/pipes/sanitize.pipe';
 import { Logger } from 'nestjs-pino';
+import { SentryFilter } from '@shared/filters/sentry.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
   app.useLogger(app.get(Logger));
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
 
   app.enableCors({
     origin: ['http://localhost:3000'],
---
+++ src/shared/filters/sentry.filter.ts
@@ -0,0 +1,15 @@
+import { Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import * as Sentry from '@sentry/nestjs';
+
+@Catch()
+export class SentryFilter extends BaseExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    if (exception instanceof HttpException && exception.getStatus() < 500) {
+      return super.catch(exception, host);
+    }
+
+    Sentry.captureException(exception);
+    super.catch(exception, host);
+  }
+}
